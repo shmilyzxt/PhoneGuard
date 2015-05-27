2956f3f2938a2968b1bf444ecd53e02d479ffede
shmilyzxt <49783121@qq.com>
2015-05-27 17:47:17 +0800

    lol

--- app/src/main/java/com/example/shmilyzxt/phoneguard/MainActivity.java
+++ app/src/main/java/com/example/shmilyzxt/phoneguard/MainActivity.java
@@ -1,18 +1,49 @@
 package com.example.shmilyzxt.phoneguard;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener ,ListFragment.OnFragmentInteractionListener {
 
+    private TextView tv;
+    private AnalogClock ac;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tv = (TextView)findViewById(R.id.tv);
+        tv.setOnClickListener(this);
+
+        activeFragment fragment = new activeFragment();
+        FragmentManager fr = getFragmentManager();
+        FragmentTransaction ft = fr.beginTransaction();
+       // ft.add(R.id.active_fragment,fragment);
+        ft.replace(R.id.active_fragment,fragment);
+        ft.commit();
+    }
+
+    @Override
+    public void onClick(View v) {
+        tv = (TextView)findViewById(R.id.tv);
+        Toast.makeText(MainActivity.this,tv.getText(),Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onFragmentInteraction(String id) {
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/shmilyzxt/phoneguard/activeFragment.java
@@ -0,0 +1,74 @@
+package com.example.shmilyzxt.phoneguard;
+
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AnalogClock;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class activeFragment extends Fragment implements View.OnClickListener{
+
+    private Button btn;
+    private AnalogClock ac;
+    private EditText et;
+
+    public activeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_active, container, false);
+        btn = (Button)view.findViewById(R.id.active_fragment_button);
+        btn.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        ac = (AnalogClock)getActivity().findViewById(R.id.analogClock);
+        et = (EditText)getView().findViewById(R.id.active_fragment_edit);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onClick(View v) {
+       // ac.setVisibility(View.INVISIBLE);
+        FragmentManager fr = getFragmentManager();
+        FragmentTransaction ft = fr.beginTransaction();
+        ft.replace(R.id.active_fragment, new ListFragment());
+        ft.addToBackStack("stack");
+        ft.commit();
+        Toast.makeText(getActivity(),btn.getText(),Toast.LENGTH_SHORT).show();
+
+        if(et.getText() != null){
+            Log.wtf("logcat",et.getText().toString());
+            Bundle bundle = new Bundle();
+            bundle.putString("text",et.getText().toString());
+        }else{
+            Log.wtf("logcat","editText is empty");
+        }
+
+
+    }
+}
