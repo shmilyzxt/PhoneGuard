54b9d504c67bf1fd16a67a8a5cedbc77de92a94d
shmilyzxt <49783121@#qq.com>
2015-05-28 21:13:32 +0800

    2015/5/28 20:56

--- app/src/main/java/com/example/shmilyzxt/phoneguard/fragments/ListFragment.java
+++ app/src/main/java/com/example/shmilyzxt/phoneguard/fragments/ListFragment.java
@@ -11,6 +11,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.example.shmilyzxt.phoneguard.R;
@@ -100,6 +101,7 @@ public class ListFragment extends Fragment implements AbsListView.OnItemClickLis
         super.onAttach(activity);
         try {
             mListener = (OnFragmentInteractionListener) activity;
+            Toast.makeText(activity,"activeFragment绑定到"+activity.toString(),Toast.LENGTH_SHORT).show();
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString()
                     + " must implement OnFragmentInteractionListener");
