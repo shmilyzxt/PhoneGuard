61ff7b5bb9461974d26b4394d83287759611e7db
shmilyzxt <49783121@qq.com>
2015-06-09 18:10:11 +0800

    有了简单socket

--- app/src/main/java/com/example/shmilyzxt/phoneguard/fragments/StaticFragment.java
+++ app/src/main/java/com/example/shmilyzxt/phoneguard/fragments/StaticFragment.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+import com.loopj.android.http.*;
 
 import com.example.shmilyzxt.phoneguard.R;
+
+import org.apache.http.Header;
 
 
 /**
@@ -15,17 +21,51 @@ import com.example.shmilyzxt.phoneguard.R;
  */
 public class StaticFragment extends Fragment {
 
+    static AsyncHttpClient client = new AsyncHttpClient();
+    final static String url = "http://www.oschina.net/action/api/news_list";
+
+    private WebView webView;
+    private View view;
 
     public StaticFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the btn_quickoption_selector for this fragment
-        return inflater.inflate(R.layout.fragment_static, container, false);
+        view = inflater.inflate(R.layout.fragment_static, container, false);
+        return view;
+    }
+
+    private void initView(){
+        //初始化view相关操作
+
+        //AsyncHttpClient方式获取数据
+        client.get(url, new TextHttpResponseHandler() {
+            @Override
+            public void onFailure(int i, Header[] headers, String s, Throwable throwable) {
+
+            }
+
+            @Override
+            public void onSuccess(int i, Header[] headers, String s) {
+                webView = (WebView)view.findViewById(R.id.webView);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.getSettings().setDefaultTextEncodingName("UTF-8");
+                webView.getSettings().setAllowContentAccess(true);
+                webView.getSettings().setSupportZoom(true);
+
+                webView.loadData(s, "text/html; charset=UTF-8", null);
+            }
+        });
     }
 
 
